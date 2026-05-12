bf1e0201ad0b5205bcba69e5660a6045d875a30a
pravallikajoseph1 <are.pravallikajoseph@gmail.com>
2026-05-12 14:19:27 +0530

    Enhance Lists.py with comments on list usage
    Added comments explaining the importance of lists in Python.

--- Lists.py
+++ Lists.py
@@ -7,3 +7,9 @@
 cities = ["Berlin", "Munich"]
 cities.append("frankfurt")
 print(cities)
+# Lists :  store multiple items in a single variable.
+# Why Lists Matter? 
+# 1. Data Organization: Lists help organize related data together.
+# 2. Flexibility: Lists can hold different data types and can grow or shrink dynamically.
+# 3. Built-in Functions: Python provides many built-in functions to manipulate lists (e.g., append, remove, sort).
+# 4. Real-world Applications: Lists are used in various applications like storing user data, managing inventory, processing data, and more.
