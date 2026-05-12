6786938ee6a774f15a3fb9b44898d98070cc3e58
pravallikajoseph1 <are.pravallikajoseph@gmail.com>
2026-05-12 14:18:44 +0530

    Add comments on function purpose and benefits
    Added comments explaining the purpose and benefits of functions.

--- Funtions.py
+++ Funtions.py
@@ -6,3 +6,9 @@
 def greet(name):
     print("Hello", name)
 greet("Joseph")
+# Function: reusable block of code that performs a specific task.
+# Why Functions Matter?
+# 1. Reusability: Write once, use multiple times.   
+# 2. Organization: Break code into smaller, manageable pieces.
+# 3. Readability: Functions with descriptive names make code easier to understand.
+# 4. Collaboration: Functions allow multiple developers to work on different parts of a project without conflicts.
